d1b9a6f1062687fc2bca6787a4a1679aed2a10c3
Xiaoxuan <ytxiaoxuan@gmail.com>
2026-06-10 10:16:50 +0200

    xxx

---
+++ GateEmbeddingTask/TwoStageMLT/cluster.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --output=logs/train_%j.out
+#SBATCH --error=logs/train_%j.err
+#SBATCH --gres=gpu:a100:1
+#SBATCH -t 48:00:00
+#SBATCH -J MLT
+#SBATCH -p gpu-amd
+#SBATCH --mail-type=begin        # send a mail when job begins
+#SBATCH --mail-type=end          # send a mail when job ends
+#SBATCH --mail-type=fail         # send a mail if job fails
+#SBATCH --mail-user=xiaoxuan.yu@scai-extern.fraunhofer.de
+ 
+# Load Miniforge and activate environment
+module load Miniforge3
+source /opt/software/easybuild/software/Miniforge3/24.1.2-0/etc/profile.d/conda.sh
+conda activate firegnn
+
+# Navigate to project directory
+cd "/home/xyu/thesis/HybridKG/GateEmbeddingTask/TwoStageMLT/"
+ 
+# Load CUDA environment
+module load CUDA
+# Note: CUDA_VISIBLE_DEVICES is automatically set by Slurm based on --gres=gpu:a100:1
+export CUDA_LAUNCH_BLOCKING=1
+
+MODEL=('hrgat') # 'hrgcn' 'rgcn' 'rgat' 'hgt' 'hgat' 'graphsage')
+
+echo "====================================================================================================="
+echo "Running Testing import script"
+echo "$(date +"%D %T")"
+echo "-------------------------------------------------------------------"
+echo "Python path:"
+which python
+
+for model in "${MODEL[@]}"; do
+    echo "Running model: $model"
+    # standard 'python' will now point to your activated firegnn environment automatically
+    /home/xyu/.conda/envs/firegnn/bin/python train.py --encoder_type "$model" --aggregator_type "$model" --num_trial 50 --epochs 500
+done
+
+echo "-------------------------------------------------------------------"
+echo "$(date +"%D %T")"
+echo "====================================================================================================="
